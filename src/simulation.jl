fc04f723fcc4f1b23cbb0a1871a48f5b20618c1c
Boryskarton <boryskopec00@gmail.com>
2020-05-21 20:47:19 +0200

    engine for simulations

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,37 +1,80 @@
-struct Universe
-    bodies::Array{Body, 1}
-end
+using LinearAlgebra
+# using Plots
 
+const G = 6.6743e-11
+
+"""
+Celestial object.
+"""
 mutable struct Body
     movable::Bool
-    x::Float64
-    y::Float64
+    position::Vector{Float64}
     radius::Float64
     mass::Float64
-    acceleration::Vector{Float64}
     velocity::Vector{Float64}
-    Body(b::Bool, x::Number, y::Number, r::Number, m::Number) =
-        new(b, x, y, r, m, [0.0; 0.0], [0.0; 0.0])
+    acceleration::Vector{Float64}
+    Body(b, p, r, m) = new(b, p, r, m, [0.0; 0.0], [0.0; 0.0])
+    Body(b, p, r, m, v) = new(b, p, r, m, v, [0.0; 0.0])
 end
 
-distance(a::Body, b::Body) = sqrt(abs2(a.x - b.x) + abs2(a.y - b.y))
+Base.show(io::IO, a::Body) = begin
+    return a.movable ?
+        print(io, "Dynamic body:\n  position: $(
+              a.position)\n  radius: $(
+              a.radius)\n  mass: $(
+              a.mass)\n  velocity: $(
+              a.velocity)\n  acceleration: $(
+              a.acceleration)") :
+        print(io, "Static body:\n  position: $(
+              a.position)\n  radius: $(
+              a.radius)\n  mass: $(
+              a.mass)\n  velocity: $(
+              a.velocity)\n  acceleration: $(
+              a.acceleration)")
+end
 
-# in progress
-update(a::Body, f::Array{Float64, 1}) = begin
+"""
+    update!(a::Body, f::Vector)
+
+Update in place object a based on given force vector.
+"""
+update!(a::Body, f::Vector) = begin
     if a.movable
         a.acceleration = f ./ a.mass
         a.velocity += a.acceleration
-        a.x += a.velocity[1]
-        a.y += a.velocity[2]
+        a.position += a.velocity
     end
 end
-# in progress
+"""
+    update!(u::Vector{Body})
 
-# in progress
-update(w::World) = begin
-    for (index, body) in enumerate(w.bodies)
-        force = [0; 0]
-        force += G * body.mass * w.bodies.mass
-    end
+Update in place all bodies in Array.
+Calculates gravity forces between bodies and updates them.
+"""
+update(u::Vector{Body}) = begin
+    masses = getfield.(u, :mass)
+    massesprod = masses .* permutedims(masses)
+    positions = getfield.(u, :position)
+    positionsdif = permutedims(positions) .- positions
+    distances = norm.(positionsdif)
+    distances2 = distances.^2
+    versors = normalize.(positionsdif)
+    forces = G * massesprod .* versors ./ distances2
+    replace!(forces, [NaN, NaN]=>[0.0, 0.0])
+    update.(u, sum(forces, dims=2))
 end
-# in progress
+
+# testing
+
+# a = Body(0, [0; 0], 0, 1000000000)
+# b = Body(1, [-1; -1], 0, 4)
+# u = [a; b]
+# scatter([a.position[1]], [a.position[2]], framestyle=:origin, color=:black, legend=:none)
+# scatter!([b.position[1]], [b.position[2]], color=:red)
+# for i in 1:15
+#     update(u)
+#     scatter!([b.position[1]], [b.position[2]], color=:red)
+# end
+# plot!()
+
+# testing
