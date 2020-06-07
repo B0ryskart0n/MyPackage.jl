9d167fa8c716352f8d71a72513a76828d81637c9
Aleksandra Winiarska <255783@student.pwr.edu.pl>
2020-06-07 12:58:40 +0200

    New draw and graph functions based on arrays

--- src/animation.jl
+++ src/animation.jl
@@ -3,28 +3,31 @@ using Plots
 
 
 """
-    draw(data::Array{Body, 2}, s::Simulation, limits::Vector) -> gif(a::Animation,::String,fps::Float64)
+    draw(data::Array{Float64, 3}, Δt::Float64) -> gif(a::Animation,::String,fps::Float64)
 
 Makes and saves an animation as animation.gif.
-Acquires limits vector in form: [xmin, xmax, ymin, ymax]
 """
-draw(data::Array{Body, 2}, s::Simulation, limits::Vector) = begin
-    Δt = s.Δt
-    a = @animate for j in 1:size(data)[2]
+draw(data::Array{Float64, 3}, Δt::Float64) = begin
+    r_max = maximum(data[2, :, :])
+    x_min = minimum(data[3, :, :]) - r_max
+    x_max = maximum(data[3, :, :]) + r_max
+    y_min = minimum(data[4, :, :]) - r_max
+    y_max = maximum(data[4, :, :]) + r_max
+    a = @animate for i in 1:size(data)[3]
         plot()
-        for i in 1:size(data)[1]
-            scatter!([data[i,j].position[1]], [data[i,j].position[2]],
-            markersize = data[i,j].radius,
+        for j in 1:size(data)[2]
+            scatter!([data[3, j, i]], [data[4, j, i]],
+            markersize = data[2, j, i],
             title = "Simulation",
-            xlim = (limits[1], limits[2]),
-            ylim = (limits[3], limits[4]),
+            xlim = (x_min, x_max),
+            ylim = (y_min, y_max),
             xlabel = "X",
             ylabel = "Y",
-            label = "Body $i")
-            plot!([data[i,j].position[1],data[i,j].position[1]+data[i,j].velocity[1]],
-            [data[i,j].position[2],data[i,j].position[2]+data[i,j].velocity[2]],
-            arrow = 0.4,
-            label = "v$i")
+            label = "Body $j")
+            plot!([data[3, j, i], data[3, j, i] + data[5, j, i]],
+            [data[4, j, i], data[4, j, i] + data[6, j, i]],
+            arrow = 0.5,
+            label = "v$j")
         end
     end
     gif(a, "animation.gif", fps=1/Δt)
@@ -32,29 +35,28 @@ end
 
 
 """
-    graph(data::Array{Body, 2}, s::Simulation, type::String, time::Vector) -> savefig(::String)
+    graph(data::Array{Body, 2}, Δt::Float64, type::String, time::Vector) -> savefig(::String)
 
 Makes and saves plots v(t) and a(t) as v.png and a.png.
 Acquires information what kind of plot to draw: "v" for v(t) and "a" for a(t).
 """
-graph(data::Array{Body, 2}, s::Simulation, type::String, time::Vector) = begin
+graph(data::Array{Float64, 3}, Δt::Float64, type::String, time::Vector) = begin
     plot()
-    Δt = s.Δt
     first_frame = ceil(Int,time[1]/Δt)
     last_frame = floor(Int,time[2]/Δt)
     Δf = last_frame - first_frame + 1
     xs = first_frame*Δt:Δt:last_frame*Δt
-    v = Array{Float64,2}(undef,Δf,size(data)[1])
-    a = Array{Float64,2}(undef,Δf,size(data)[1])
-    for i in 1:size(data)[1]
-        for j in first_frame+1:last_frame+1
-            v[j,i] = sqrt(data[i,j].velocity[1]^2+data[i,j].velocity[2]^2)
-            a[j,i] = sqrt(data[i,j].acceleration[1]^2+data[i,j].acceleration[2]^2)
+    v = Array{Float64,2}(undef,size(data)[2],Δf)
+    a = Array{Float64,2}(undef,size(data)[2],Δf)
+    for i in 1:size(data)[2]
+        for j in 1:Δf
+            v[i, j] = sqrt(data[5, i, j + first_frame]^2+data[6, i, j + first_frame]^2)
+            a[i, j] = sqrt(data[7, i, j + first_frame]^2+data[8, i, j + first_frame]^2)
         end
     end
     if type == "v" #dla v(t)
-        for i in 1:size(v)[2]
-            plot!(xs,v[:,i],
+        for i in 1:size(v)[1]
+            plot!(xs, v[i, :],
             title = "v(t)",
             xlabel = "t[s]",
             ylabel = "v(t)[m/s]",
@@ -62,8 +64,8 @@ graph(data::Array{Body, 2}, s::Simulation, type::String, time::Vector) = begin
         end
         savefig("v.png")
     elseif type == "a" #dla a(t)
-        for i in 1:size(a)[2]
-            plot!(xs,a[:,i],
+        for i in 1:size(a)[1]
+            plot!(xs, a[i, :],
             title = "a(t)",
             xlabel = "t[s]",
             ylabel = "a(t)[m/s]",
@@ -75,16 +77,12 @@ end
 
 
 # test
-#Do rysowania animacji potrzebujemy wektora limits, gdzie jest on postaci:
-# [xmin,xmax,ymin,ymax] <- xmin...ymax podaje użytkownik (w gui)
-#Trzeba to w odpowiedzni sposób uwzględnić podczas wywoływania draw(...)
-# a = Body(1, [0; 0], 5, 100000000000,[0,-1])
-# b = Body(1, [-5; -5], 2, 5,[0,0.5])
-# c = Body(1, [5; 5], 2, 4,[0,-0.5])
-# u = [a; b; c]
-# s = Simulation(u,0.5,25,0)
-# r = run!(s)
-# graph(r,s,"a",[0,5]) #a(t)
-# graph(r,s,"v",[0,5]) #v(t)
-# draw(r,s,[-5,5,-5,5]) #<= wersja testowa dla sił; docelowo draw(s,limit)
+# b = [1.5; 4.0; 5.0; 0.0; 0.5; 0.0; 0.0; 0.0]
+# c = [100000000000.0; 8.0; 5.0; 5.0; 0.0; 0.0; 0.0; 0.0]
+# bodies = [b c]
+# t = 0.1
+# r = run(bodies, 50.0, t)
+# graph(r,t,"a",[0,40]) #a(t)
+# graph(r,t,"v",[0,40]) #v(t)
+# draw(r,t)
 #test
